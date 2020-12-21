0c7bbfa55433b28c1c5fccffe82eee430913417c
tranhoangphuc2005 <phuctranhoang125@gmail.com>
2020-12-21 20:54:39 +0700

    21 dec 20:55

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Css/Info.css";
-import { Button } from "semantic-ui-react";
+import { Button, Card } from "semantic-ui-react";
 
 const Info = (props) => {
-  console.log(props);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       {props.tabs.map((tab) => (
-        <button class="ui button">{tab.company}</button>
+        <div>
+          <button
+            class="ui button"
+            onClick={() => {
+              if (isOpen === false) {
+                setIsOpen(true);
+              } else {
+                setIsOpen(false);
+              }
+            }}
+          >
+            {tab.company}
+          </button>
+          <Card
+            header={tab.title}
+            meta={tab.dates}
+            description={tab.duties}
+            isOpen={true}
+          />
+        </div>
       ))}
     </>
   );
